85b7700da6e0d2d129fc931be7defa0ca629e528
andreisemianiuk <Andrei.Semianiuk@rhino.ai>
2026-02-14 00:14:51 +0300

    Add logs to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+echo "PORT is: ${PORT:-not set}"
+echo "Before sed:"
+head -3 /etc/nginx/conf.d/default.conf
 sed -i "s/listen 80;/listen ${PORT:-80};/" /etc/nginx/conf.d/default.conf
+echo "After sed:"
+head -3 /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
